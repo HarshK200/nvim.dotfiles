e30e482f0bf5402858917b496fc21194a43fea83
HarshK200 <harshkapadia04@gmail.com>
2024-08-07 15:38:34 +0530

    added neotree and telescope find hidden/env files

--- lua/kouShinji/plugins/telescope.lua
+++ lua/kouShinji/plugins/telescope.lua
@@ -11,6 +11,11 @@ return {
         local actions = require("telescope.actions")
 
         telescope.setup({
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
             defaults = {
                 --path_display = { "truncate" },
                 mappings = {
@@ -24,7 +29,7 @@ return {
 
         -- My custom keymaps for telescope
         local keymap = vim.keymap -- for conciseness
-        keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<C-f>", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files,-u <cr>", { desc = "Fuzzy find files in cwd" })
         keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
         keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
         keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", {desc = "Find currently open buffers"})
