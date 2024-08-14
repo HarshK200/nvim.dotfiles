a9bab0a5ff3100f020e743ed04eb845595646093
HarshK200 <harshkapadia04@gmail.com>
2024-08-14 19:15:57 +0530

    highlight on yank autocommand added

--- lua/kouShinji/autocommands.lua
+++ lua/kouShinji/autocommands.lua
@@ -1,10 +1,18 @@
 -- Set indenting to 2 spaces for specific filetypes
 vim.api.nvim_exec(
 	[[
-  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html setlocal shiftwidth=2
-  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html setlocal softtabstop=2
-  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html setlocal tabstop=2
-  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html setlocal expandtab
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html,css,json setlocal shiftwidth=2
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html,css,json setlocal softtabstop=2
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html,css,json setlocal tabstop=2
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact,html,css,json setlocal expandtab
 ]],
 	false
 )
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlights the text when yanked",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
