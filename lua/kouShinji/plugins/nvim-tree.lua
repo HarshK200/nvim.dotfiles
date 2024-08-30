e579dd750028f0e40481cd21b7aaf83e77b6e74a
HarshK200 <harshkapadia04@gmail.com>
2024-08-31 03:02:29 +0530

    cleared the file filters and lsp diagnostics added

--- lua/kouShinji/plugins/nvim-tree.lua
+++ lua/kouShinji/plugins/nvim-tree.lua
@@ -25,8 +25,24 @@ return {
 			renderer = {
 				group_empty = true,
 			},
+			diagnostics = {
+				enable = true,
+				show_on_dirs = false,
+				show_on_open_dirs = true,
+				debounce_delay = 50,
+				severity = {
+					min = vim.diagnostic.severity.HINT,
+					max = vim.diagnostic.severity.ERROR,
+				},
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
+				},
+			},
 			filters = {
-				dotfiles = true,
+				enable = false,
 			},
 		})
 	end,
