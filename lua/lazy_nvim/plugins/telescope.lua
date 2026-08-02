70decb052a7c294c0d2d5b35c145ece5fa03607d
HarshK200 <harshkapadia04@gmail.com>
2026-08-03 01:00:05 +0530

    Removed conflicting keybind telescope

--- lua/lazy_nvim/plugins/telescope.lua
+++ lua/lazy_nvim/plugins/telescope.lua
@@ -39,6 +39,5 @@ return {
 		vim.keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor cwd" })
-		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
