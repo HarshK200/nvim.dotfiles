225200ccc45179ba9802b71efacae3e1a99cf8c8
HarshK200 <harshkapadia04@gmail.com>
2026-08-19 02:48:50 +0530

    Fixed trouble.nvim bug

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,12 @@
 return {
 	"folke/trouble.nvim",
+	opts = {},
 	-- lazy load on following key press
-	keys = { "<leader>xx", "<leader>xX", "<leader>cs" },
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", mode = "n" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", mode = "n" },
+		{ "<leader>cs", "<cmd>Trouble diagnostics symbols toggle focus=false<cr>", mode = "n" },
+	},
 	-- lazy load when command Trouble is used
 	cmd = "Trouble",
-	config = function()
-		-- keymaps
-		vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>")
-		vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>")
-		vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>")
-	end,
 }
