97c79095544317b91bafb96a20a063ca25de44ea
HarshK200 <harshkapadia04@gmail.com>
2026-09-16 16:26:06 +0530

    changed completion open keymap

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -18,7 +18,7 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<S-Tab>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<C-y>"] = cmp.mapping.confirm({ select = false }),
 			}),
